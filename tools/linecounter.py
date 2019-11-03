1f7a0d9e18e376094e9fd95fde594833e1e18ac8
RobbeDGreef <robbedegreef@gmail.com>
2019-11-03 22:56:29 +0100

    Added filecounter

--- tools/linecounter.py
+++ tools/linecounter.py
@@ -1,7 +1,8 @@
 import os
 
 lines = 0
-v = True
+filecount = 0
+v = False
 extensions = [".h", ".c", ".asm", ".py", ".ld", ".md"]
 
 noreaddirs = ["examples", ".git", "sysroot", "sysroot2", "sysroot3", ".testing"]
@@ -37,12 +38,14 @@ for subdir, dirs, files in os.walk(os.getcwd()):
 		for ext in extensions:
 			if file.endswith(ext):
 				# how many lines
+				filecount += 1
 				if v:
 					print("    reading file: ", file)
 				with open(os.path.join(subdir, file), "r") as f:
 					lines += len(f.read().split("\n"))
 
 print("total lines: ", lines)
+print("total files: ", filecount)
 try:
 	size = os.path.getsize(os.path.join(os.getcwd(), "os-image.bin"))
 	print("os image size: ", str(size) + "B", "(" + str(round(size/1024)) + "kB)")
