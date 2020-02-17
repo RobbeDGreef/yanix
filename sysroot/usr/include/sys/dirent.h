adc7fb8bef1162b1b0f03ec5db5847326e3200fc
RobbeDGreef <robbedegreef@gmail.com>
2020-02-17 22:52:39 +0100

    Sysroot has posix support

--- sysroot/usr/include/sys/dirent.h
+++ sysroot/usr/include/sys/dirent.h
@@ -1,13 +1,27 @@
-/* <dirent.h> includes <sys/dirent.h>, which is this file.  On a
-   system which supports <dirent.h>, this file is overridden by
-   dirent.h in the libc/sys/.../sys directory.  On a system which does
-   not support <dirent.h>, we will get this file which uses #error to force
-   an error.  */
+#ifndef _SYS_DIRENT_H
+#define _SYS_DIRENT_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-#error "<dirent.h> not supported"
-#ifdef __cplusplus
-}
+/* Yanix customized code */
+
+/**
+ * @brief      dirent structure according to POSIX standards
+ */
+struct dirent {
+	unsigned long	d_ino;		/* The inode number */
+	unsigned long 	d_off;		/* The offset to the next dirent */ 
+	unsigned short 	d_reclen;	/* Length of this dirent structure in bytes */
+
+	char 		d_type;			/* The filetype */
+	char 		d_name[];		/* The filename (null terminated c string) */
+};
+
+typedef struct {
+	int 	dd_fd;		/* Directory file descriptor */
+	int 	dd_loc;		/* Position in buffer in bytes */
+	int 	dd_seek;
+	char 	*dd_buf;	/* Buffer */
+	int 	dd_len;		/* Buffer length in bytes*/
+	int 	dd_size;	/* Amount of data in buffer in bytes*/
+} DIR;
+
 #endif
--- sysroot/usr/include/sys/fcntl.h
+++ sysroot/usr/include/sys/fcntl.h
@@ -1,4 +1,5 @@
 #ifndef _SYS_FCNTL_H_
 #define _SYS_FCNTL_H_
 #include <sys/_default_fcntl.h>
+int fcntl(int fd, int cmd, ...);
 #endif
