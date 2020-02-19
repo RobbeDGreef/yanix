cf092cb3c11e1ac919e954c4a99d48048c5334d1
RobbeDGreef <robbedegreef@gmail.com>
2020-02-19 21:25:59 +0100

    Debugging

--- kernel/yanix/kernel.c
+++ kernel/yanix/kernel.c
@@ -1,12 +1,19 @@
 #include <kernel.h>
 #include <yanix/exec.h>
 
+/* Debugging */
+
+#include <fs/vfs_node.h>
+
+extern vfs_node_t *g_vfs_root;
+
+/* Debugging */
+
 /**
  * @brief      Kernel main loop
  */
 void kernel_main()
 {
 	printk(KERN_INFO "kernel boot up procedure completed\n");
-
 	execve_user("/bin/testfile4",0,0);
 }
