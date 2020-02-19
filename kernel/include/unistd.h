b13ca38b7938b3df298c28aaf8200ca7ee144e5e
RobbeDGreef <robbedegreef@gmail.com>
2020-02-19 21:29:08 +0100

    Initial commit of unistd header, sets SEEK_* constants

---
+++ kernel/include/unistd.h
@@ -0,0 +1,16 @@
+#ifndef _UNISTD_H
+#define _UNISTD_H
+
+/* @todo: Incomplete */
+
+#ifndef SEEK_SET
+#define SEEK_SET    0   /* set file offset to offset */
+#endif
+#ifndef SEEK_CUR
+#define SEEK_CUR    1   /* set file offset to current plus offset */
+#endif
+#ifndef SEEK_END
+#define SEEK_END    2   /* set file offset to EOF plus offset */
+#endif
+
+#endif /* _UNISTD_H */
