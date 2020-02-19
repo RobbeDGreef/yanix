8c34808259ce6f2b5466484236081e2349fe365c
RobbeDGreef <robbedegreef@gmail.com>
2020-02-19 21:25:21 +0100

    Corrected O_* defines

--- kernel/include/fcntl.h
+++ kernel/include/fcntl.h
@@ -3,17 +3,18 @@
 
 //@todo: incomplete 
 
-#define O_ACCMODE	3
-#define O_RDONLY	0
-#define O_WRONLY	1
-#define O_RDWR		2
-#define O_CREAT		100
-#define O_EXCL		200
-#define O_NOCTTY	400
-#define O_TRUNC		1000
-#define O_APPEND	2000
-#define O_NONBLOCK	4000
-#define O_DSYNC		10000
-#define FASYNC		20000
+#define O_ACCMODE	0x3
+#define O_RDONLY	0x0
+#define O_WRONLY	0x1
+#define O_RDWR		0x2
+#define O_CREAT		0x100
+#define O_EXCL		0x200
+#define O_NOCTTY	0x400
+#define O_TRUNC		0x1000
+#define O_APPEND	0x2000
+#define O_NONBLOCK	0x4000
+#define O_DSYNC		0x10000
+#define FASYNC		0x20000
+#define O_DIRECTORY 0x200000
 
 #endif
