4a9444805cf7e8a445cc903e3881f71a3a77ba18
RobbeDGreef <robbedegreef@gmail.com>
2020-03-01 10:57:32 +0100

    Changed data type of execve args

--- kernel/include/yanix/exec.h
+++ kernel/include/yanix/exec.h
@@ -1,7 +1,7 @@
 #ifndef EXEC_H
 #define EXEC_H
 
-int execve_user(const char *filename, char *const argv[], char *const envp[]);
-int execve(const char *filename, char *const argv[], char *const envp[]);
+int execve_user(const char *filename, const char * argv[], char *const envp[]);
+int execve(const char *filename, const char * argv[], char *const envp[]);
 
 #endif
