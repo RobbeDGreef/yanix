6c505f95df64ccabfbccf8dbbe2d923b02f99682
RobbeDGreef <robbedegreef@gmail.com>
2020-03-01 10:56:51 +0100

    implemented get_filedescriptor_from_node(), can find file_descriptor structs by node

--- kernel/include/yanix/ds/fd_vector.h
+++ kernel/include/yanix/ds/fd_vector.h
@@ -2,6 +2,7 @@
 #define _YANIX_DS_FD_VECTOR_H
 
 #include <fs/filedescriptor.h>
+#include <fs/vfs_node.h>
 
 #define VECTOR_BEGIN_AMOUNT 8
 
@@ -17,5 +18,6 @@ vector_t *vector_copy(vector_t *cp);
 int vector_clear(vector_t *vec, int fd);
 int vector_add(vector_t *vec, struct file_descriptor fd);
 struct file_descriptor *vector_get(vector_t *vec, int fd);
+struct file_descriptor *vector_get_node(vector_t *vec, vfs_node_t *node);
 
 #endif /* _YANIX_DS_FD_VECTOR_H */
