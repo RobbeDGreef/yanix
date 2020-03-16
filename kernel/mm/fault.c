048c1f6e261c2e6663151514b221d6411f2370f4
RobbeDGreef <robbedegreef@gmail.com>
2020-03-16 10:46:59 +0100

    Improved page_fault system, more error messages

--- kernel/mm/fault.c
+++ kernel/mm/fault.c
@@ -38,8 +38,6 @@ void error_message(offset_t addr, registers_t *regs, int present, int rw, int us
 		if (rw) 	  printk("read write, ");
 		if (us) 	  printk("user-mode, ");
 		if (reserved) printk("reserved, ");
-		printk("\nframe: %x\n", get_page(addr, 0, get_current_dir()));
-		printk_hd(get_page(addr, 0, get_current_dir()), 200);
 		debug_handler(regs);
 		send_sig(SIGKILL);
 		for(;;);
@@ -52,28 +50,31 @@ void page_fault(registers_t *regs)
 	asm volatile("mov %%cr2, %0":"=r"(faulting_address));
 
 	/* 0 means that the page was not present in regs->err_code & 0x1 */
-	int not_present = !(regs->err_code & 0x1);	/* Wheter the page was present */
+	int not_present = !(regs->err_code & 0x1);	/* Whether the page was present */
 	int rw = regs->err_code & 0x2;			/* whether it was a write operation or not */
 	int us = regs->err_code & 0x4;			/* processor in ring 3 */
 	int reserved = regs->err_code & 0x8;	/* Overwritten CPU-reserved bits of a page entry */
 
-	printk("Page fault occured at: ");
-
+	/* Check wheter it was part of a heap and should be zero'd out */
+	struct heap *heap = is_addr_in_heap(faulting_address);
+	
 	if (not_present)
 	{
-		/* Check wheter it was part of a heap and should be zero'd out */
-		struct heap *heap = is_addr_in_heap(faulting_address);
 		if (heap)
 			zero_page(faulting_address, get_kernel_dir(), heap->usermode);
 
 		/* Check wheter it was part of the user space program running
 		 * with the program break and zero it out */
 		else if (faulting_address >= get_current_task()->program_start && faulting_address <= get_current_task()->program_break)
-			zero_page(faulting_address, get_current_task()->directory, get_current_task()->ring);
+			zero_page(faulting_address, get_current_dir(), get_current_task()->ring);
 
 		/* Otherwise call the error message */
 		else
 			error_message(faulting_address, regs, not_present, rw, us, reserved);
-
+		
+	} 
+	else
+	{
+		error_message(faulting_address, regs, not_present, rw, us, reserved);
 	}
 }
