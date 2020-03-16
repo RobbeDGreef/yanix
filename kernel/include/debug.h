10aafa5901a275546685749bb6f8b50adaeda710
RobbeDGreef <robbedegreef@gmail.com>
2020-03-16 10:43:26 +0100

    Added more debugging capabilities, more info / more functions to get info

--- kernel/include/debug.h
+++ kernel/include/debug.h
@@ -19,13 +19,19 @@ void printk_hd(void *ptr, size_t size);
 #define get_return_address __builtin_return_address
 
 void debug_handler(registers_t *regs);
+void debug_handler_task();
 void dump_stacktrace();
 void debug_print_m(char *str, ...);
 void debug_print(char *str);
 void debug_print_hex(unsigned int val);
+void debug_printk(const char* __restrict fmt, ...);
 
 void hang();
+void print_return_address();
+void print_stack();
 
 int check_vfs_initialised();
+int random_complex_calc(int x);
+
 
 #endif
