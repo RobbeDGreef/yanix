2e827e8e4eaaa0fff79b756369b193db1e2bd0ca
RobbeDGreef <robbedegreef@gmail.com>
2020-03-29 11:16:54 +0200

    Fixed user and kernel stacks to specific location

--- kernel/include/yanix/stack.h
+++ kernel/include/yanix/stack.h
@@ -4,11 +4,13 @@
 #include <stdint.h>
 #include <mm/paging.h>
 
-#define DISIRED_STACK_LOCATION 	0x1000000
-#define STACK_SIZE				0x1000
-#define STACK_LOCATION			0x9000
-#define KERNEL_STACK_SIZE 		0x1000
-#define USER_STACK_SIZE			0x4000 	// 16Kib 
+#define DISIRED_STACK_LOCATION 		0x1000000
+#define DISIRED_USER_STACK_LOC		0x2000000
+#define DISIRED_KERNEL_STACK_LOC	0x3000000
+#define STACK_SIZE					0x1000
+#define STACK_LOCATION				0x9000
+#define KERNEL_STACK_SIZE 			0x1000
+#define USER_STACK_SIZE				0x4000 	// 16Kib 
 
 
 extern uint32_t g_initial_esp;
@@ -24,5 +26,6 @@ void init_stack(uint32_t stack_location);
  * @brief      Initializes the stack with paging and bind it to the disired virtual address
  */
 void init_paging_stack();
+void set_user_stack();
 
 #endif /* stack.h */
