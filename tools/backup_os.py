074e051d39f778b69b573cc24a6e3bbf120f6bf7
RobbeDGreef <robbedegreef@gmail.com>
2020-03-29 11:13:35 +0200

    Excluded toolchain folder from backup system to reduce backup size

--- tools/backup_os.py
+++ tools/backup_os.py
@@ -4,4 +4,4 @@ import os
 print("Backuping os...")
 print("Note that the name will start with OS_BACKUP_DAY_MONTH")
 name = input("Name: ")
-os.system("zip -r ../backups/`date +'os_backup_%d_%m_" + name + "'` ./")
+os.system("zip -x 'toolchain*' -r ../backups/`date +'os_backup_%d_%m_" + name + "'` ./")
