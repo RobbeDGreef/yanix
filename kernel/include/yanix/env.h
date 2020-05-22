af5bcde4d8b2fa6efc8e2b949a6473ff925b5c71
RobbeDGreef <robbedegreef@gmail.com>
2020-05-22 17:23:26 +0200

    EXECVE_MAX_ARGS and EXECVE_MAX_ENV defines added

--- kernel/include/yanix/env.h
+++ kernel/include/yanix/env.h
@@ -1,6 +1,9 @@
 #ifndef _ENV_H
 #define _ENV_H
 
+#define EXECVE_MAX_ARGS	512
+#define EXECVE_MAX_ENV	512
+
 char **make_envvars();
 char **make_args(int amount, ...);
 char **combine_args_env(char **argv, char **env);
