7f3a471776af5b2bfa0ea9d5653adfb9574a44d5
RobbeDGreef <robbedegreef@gmail.com>
2020-05-22 17:24:35 +0200

    main kernel loop spawns shell

--- kernel/yanix/kernel.c
+++ kernel/yanix/kernel.c
@@ -22,13 +22,10 @@ void kernel_main()
 {
 	printk(KERN_INFO "kernel boot up procedure completed\n");
 	char **envvars = make_envvars();
-	const char **args = (const char **) make_args(3, "/bin/smlrc", "/etc/hello_nostd.c", "/testout.asm");
+	const char **args = (const char **) make_args(1, "/bin/yash");
 
-	char *str = "testing here \e[1;5H homed\n";
-	vfs_write_fd(1, str, strlen(str));
-
-	//if (fork() == 0)
-	//{
-	//	execve_user("/bin/clear", args, envvars);
-	//}
+	if (fork() == 0)
+	{
+		execve_user("/bin/yash", args, (const char**) envvars);
+	}
 }
