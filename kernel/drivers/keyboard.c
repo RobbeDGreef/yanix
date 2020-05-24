3539be21b07ca63160f3d5273d38aef2648e11b6
RobbeDGreef <robbedegreef@gmail.com>
2020-05-24 20:59:23 +0200

    Created generic keyboard abstraction

---
+++ kernel/drivers/keyboard.c
@@ -0,0 +1,179 @@
+#include <drivers/keyboard.h>
+#include <proc/tasking.h>
+#include <fs/vfs.h>
+#include <libk/string.h>
+#include <mm/heap.h>
+#include <debug.h>
+
+char *keymap_normal = NULL;
+char *keymap_shift = NULL;
+char *keymap_altgr = NULL;
+
+struct key_mod g_modifier;
+
+#define LSHIFT_D 0x2a
+#define LSHIFT_U 0xaa
+
+#define CAPS_LCK 0x3a
+
+#define RSHIFT_D 0x36
+#define RSHIFT_U 0xb6
+
+#define ALTGR_D  22456
+#define ALTGR_U  224184
+
+#define RETURN_D 0x1C
+#define LCTRL_D  0x1D
+#define LCTRL_U  0x9D
+
+#define BACKSPACE_D 0xE
+#define BACKSPACE_U 0xBE
+
+static int modifiers(int scancode)
+{
+	switch (scancode)
+	{
+	case LSHIFT_U:
+	case RSHIFT_U:
+		g_modifier.shift = 0;
+		return 0;
+
+	case LSHIFT_D:
+	case RSHIFT_D:
+		g_modifier.shift = 1;
+		return 0;
+
+	case CAPS_LCK:
+		g_modifier.shift = g_modifier.shift ? 0 : 1;
+		return 0;
+
+	case ALTGR_D:
+		g_modifier.altgr = 1;
+		return 0;
+
+	case ALTGR_U:
+		g_modifier.altgr = 0;
+		return 0;
+
+	case LCTRL_U:
+		g_modifier.ctrl = 0;
+		return 0;
+
+	case LCTRL_D:
+		g_modifier.ctrl = 1;
+		return 0;
+
+	case BACKSPACE_D:
+		return 0x8;
+
+	case RETURN_D:
+		return '\n';
+	}
+
+	return -1;
+}
+
+static int getkey(int scancode)
+{
+	int c;
+	if ((c = modifiers(scancode)) >= 0)
+		return c;
+
+	if (scancode > 128)
+		return 0;
+
+	if (g_modifier.shift)
+		return keymap_shift[scancode];
+	if (g_modifier.altgr)
+		return keymap_altgr[scancode];
+	if (keymap_normal)
+		return keymap_normal[scancode];
+
+	return 0;
+	//return '?';
+}
+
+static int combination(int c)
+{
+	if (g_modifier.ctrl && c == 'c')
+	{
+		send_sig(SIGINT);
+		return 1;
+	}
+
+	return 0;
+}
+
+int send_scancode(int scancode)
+{
+	char c = getkey(scancode);
+
+	int interuptchar = '^';
+	if (combination(c))
+		vfs_write_fd(0, &interuptchar, 1);
+
+	if (c)
+		vfs_write_fd(0, &c, 1);
+	return 0;
+}
+
+static void set_keymap(char *map, char *buf, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (buf[i] == '\n')
+			map[i] = i;
+		else
+		{
+			map[i] = buf[i];
+			buf++;
+		}
+	}
+}
+
+int init_keyboard()
+{
+	keymap_normal = kmalloc(KEYMAP_SIZE);
+	keymap_shift  = kmalloc(KEYMAP_SIZE);
+	keymap_altgr  = kmalloc(KEYMAP_SIZE);
+	
+	memset(&g_modifier, 0, sizeof(struct key_mod));
+
+	char *keymap = kmalloc(32);
+	struct file *fp = vfs_open(KEYMAP_FILE, 0, 0);
+	vfs_read(fp, keymap, fp->filesize);
+	vfs_close(fp);
+
+	char *file = kmalloc(256);
+	char *buf  = kmalloc(KEYMAP_FILE_SIZE);
+
+	strcpy_s(file, KEYMAP_FILE_PREFIX, 256);
+	strcat(file, keymap);
+	strcat(file, "-normal");
+	fp = vfs_open(file, 0, 0);
+	vfs_read(fp, buf, fp->filesize);
+	set_keymap(keymap_normal, buf, fp->filesize);
+	vfs_close(fp);
+
+	strcpy_s(file, KEYMAP_FILE_PREFIX, 256);
+	strcat(file, keymap);
+	strcat(file, "-shift");
+	fp = vfs_open(file, 0, 0);
+	vfs_read(fp, buf, fp->filesize);
+	set_keymap(keymap_shift, buf, fp->filesize);
+	vfs_close(fp);
+
+	strcpy_s(file, KEYMAP_FILE_PREFIX, 256);
+	strcat(file, keymap);
+	strcat(file, "-altgr");
+	fp = vfs_open(file, 0, 0);
+	vfs_read(fp, buf, fp->filesize);
+	set_keymap(keymap_altgr, buf, fp->filesize);
+	vfs_close(fp);
+
+	kfree(keymap);
+	kfree(file);
+	kfree(buf);
+
+	return 0;
+}
---
+++ kernel/include/drivers/keyboard.h
@@ -0,0 +1,20 @@
+#ifndef _KEYBOARD_H
+#define _KEYBOARD_H
+
+#define KEYMAP_FILE "/etc/default/keymap"
+#define KEYMAP_FILE_PREFIX "/etc/keyboard/"
+#define KEYMAP_SIZE 128
+#define KEYMAP_FILE_SIZE 256
+
+struct key_mod
+{
+	int ctrl;
+	int shift;
+	int alt;
+	int altgr;
+};
+
+int send_scancode(int scancode);
+int init_keyboard();
+
+#endif /* _KEYBOARD_H */
