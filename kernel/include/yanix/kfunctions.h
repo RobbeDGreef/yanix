84cb925038148e8a1a77e1cb820a87b4587e62e9
RobbeDGreef <robbedegreef@gmail.com>
2020-05-25 13:54:24 +0200

    Removed nonexistent function declaration

--- kernel/include/yanix/kfunctions.h
+++ kernel/include/yanix/kfunctions.h
@@ -8,10 +8,5 @@ void sleep(unsigned int milliseconds);
 void askKeyboardHook(void (*keyhook)(char key));
 void shutdown(int errorcode);
 
-/**
- * @brief      Exits the current process
- */
-int sys_exit(int status);
-
 char *int_to_str(unsigned int val);
 #endif
