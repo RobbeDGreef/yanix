a1d126077f61122a01024a82cb0aed1d451b19d3
RobbeDGreef <robbedegreef@gmail.com>
2020-06-02 20:47:18 +0200

    Created kernel config file

---
+++ kernel/config.h
@@ -0,0 +1,9 @@
+#ifndef _YANIX_CONFIG_H
+#define _YANIX_CONFIG_H
+
+#define OS_NAME    "Yanix"
+#define OS_VERSION "0.0.1"
+#define OS_RELEASE "Yanix OS 0.0.1 (2020-5-31)"
+#define HW_IDENT   "i686"
+
+#endif /* _YANIX_CONFIG_H */
