ba22d9400e251d59bfacecd208e138a2fc0f6e25
RobbeDGreef <robbedegreef@gmail.com>
2020-06-04 15:37:30 +0200

    Expanded keymap_file_size macro because it was to small

--- kernel/include/drivers/keyboard.h
+++ kernel/include/drivers/keyboard.h
@@ -4,7 +4,7 @@
 #define KEYMAP_FILE        "/etc/default/keymap"
 #define KEYMAP_FILE_PREFIX "/etc/keyboard/"
 #define KEYMAP_SIZE        128
-#define KEYMAP_FILE_SIZE   256
+#define KEYMAP_FILE_SIZE   256*2
 
 struct key_mod
 {
