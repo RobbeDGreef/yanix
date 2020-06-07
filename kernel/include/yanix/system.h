2f180d5b83da4754c6c04474651f8182b81346ce
RobbeDGreef <robbedegreef@gmail.com>
2020-06-07 11:07:45 +0200

    Created sysinfo structs

--- kernel/include/yanix/system.h
+++ kernel/include/yanix/system.h
@@ -8,6 +8,23 @@ struct kern_sysinfo
 	char *release;
 	char *version;
 	char *machine;
+
+	unsigned long totalram;
+	unsigned long totalswap;
+};
+
+struct us_sysinfo
+{
+	long uptime;
+	unsigned long loads[3];
+	unsigned long totalram;
+	unsigned long freeram;
+	unsigned long sharedram;
+	unsigned long bufferram;
+	unsigned long totalswap;
+	unsigned long freeswap;
+	unsigned short procs;
+	char _pad[22];
 };
 
 extern struct kern_sysinfo g_system;
