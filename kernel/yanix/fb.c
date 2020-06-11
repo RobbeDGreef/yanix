1bc7025ae77f80f197eb627d846850b9a1123aa9
RobbeDGreef <robbedegreef@gmail.com>
2020-06-11 19:45:17 +0200

    Started on fb interface for libgfx

---
+++ kernel/yanix/fb.c
@@ -0,0 +1,26 @@
+#include <yanix/fb.h>
+#include <drivers/video/video.h>
+#include <fs/vfs_node.h>
+#include <sys/types.h>
+#include <yanix/ioctl.h>
+#include <libk/string.h>
+
+int fb_cmd(int request, char *args)
+{
+	switch (request)
+	{
+	case FBIOGET_FSCREENINFO:
+	case FBIOGET_VSCREENINFO:
+		return video_get_screeninfo((struct fb_screeninfo *) args);
+	}
+
+	return -1;
+}
+
+ssize_t fb_write(vfs_node_t *node, unsigned int off, const void *buf,
+                 size_t size)
+{
+	void *addr = (void *) (video_get_screen_fb() + off);
+	memcpy(addr, buf, size);
+	return size;
+}
