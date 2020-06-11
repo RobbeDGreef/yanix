208bb64e30bf49e947fb49541eeb8eb8c6d28282
RobbeDGreef <robbedegreef@gmail.com>
2020-06-11 19:43:30 +0200

    Moved some header files around

---
+++ kernel/include/kernel/system.h
@@ -0,0 +1,34 @@
+#ifndef _SYSTEM_H
+#define _SYSTEM_H
+
+struct kern_sysinfo
+{
+	char *hostname;
+	char *sysname;
+	char *release;
+	char *version;
+	char *machine;
+
+	unsigned long totalram;
+	unsigned long totalswap;
+};
+
+struct us_sysinfo
+{
+	long uptime;
+	unsigned long loads[3];
+	unsigned long totalram;
+	unsigned long freeram;
+	unsigned long sharedram;
+	unsigned long bufferram;
+	unsigned long totalswap;
+	unsigned long freeswap;
+	unsigned short procs;
+	char _pad[22];
+};
+
+extern struct kern_sysinfo g_system;
+
+int init_sysinfo();
+
+#endif /* _SYSTEM_H */
