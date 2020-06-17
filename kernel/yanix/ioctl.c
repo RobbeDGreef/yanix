51b9a81f08172e037d31fe60cb637c63072d7117
RobbeDGreef <robbedegreef@gmail.com>
2020-06-17 20:51:32 +0200

    ioctl correctly implemented

--- kernel/yanix/ioctl.c
+++ kernel/yanix/ioctl.c
@@ -1,8 +1,9 @@
 #include <yanix/sys/ioctl.h>
 #include <fs/filedescriptor.h>
 #include <debug.h>
+#include <stdarg.h>
 
-int ioctl(int fd, int request, char *args)
+int _ioctl(int fd, int request, char *args)
 {
 	debug_printk("ioctl\n");
 	struct file_descriptor *fp = get_filedescriptor(fd);
@@ -11,4 +12,13 @@ int ioctl(int fd, int request, char *args)
 		return -1;
 
 	return fp->node->cmd(request, args);
+}
+
+int ioctl(int fd, int request, ...)
+{
+	va_list vl;
+	va_start(vl, request);
+	int ret = _ioctl(fd, request, va_arg(vl, char *));
+	va_end(vl);
+	return ret;
 }
