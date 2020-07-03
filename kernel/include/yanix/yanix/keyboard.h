a47f6f47e0881ab504948771b26e0eb9539f5b89
RobbeDGreef <robbedegreef@gmail.com>
2020-07-03 21:33:20 +0200

    Started work on event pump system

--- kernel/include/yanix/yanix/keyboard.h
+++ kernel/include/yanix/yanix/keyboard.h
@@ -1,15 +1,17 @@
 #ifndef _YANIX_YANIX_KEYBOARD_H
 #define _YANIX_YANIX_KEYBOARD_H
 
+#include <sys/types.h>
+
 #define KEYMODE_PRESSED    0
 #define KEYMODE_RELEASED   1
 #define KEYMODE_AUTOREPEAT 2
 
-struct input_kb_pkt
+struct kb_pkt
 {
 	uint32_t scancode;   /* Scancode of pressed key */
 	uint16_t mode;       /* Mode of press */
-	uint16_t translated; /* Translated key */
+	uint16_t key; 		 /* Translated key */
 };
 
 #endif /* _YANIX_YANIX_KEYBOARD_H */
