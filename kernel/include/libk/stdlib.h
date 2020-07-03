4a778573a16ef4aca85759b02d5bf2795466d791
RobbeDGreef <robbedegreef@gmail.com>
2020-07-03 21:26:23 +0200

    Properly formatted libk/stdlib header and added itoa() function

--- kernel/include/libk/stdlib.h
+++ kernel/include/libk/stdlib.h
@@ -3,29 +3,38 @@
 
 //#include <sys/cdefs.h>
 #include <limits.h>
+#include <yanix/yanix/limits.h>
 
-// standard library
+/* standard library */
 
 #ifndef NULL
 	#define NULL 0
 #endif
+
 #define EXIT_FAILURE 0
 #define EXIT_SUCCESS 1
-#define RAND_MAX     UINT_MAX // this is defined in limits.h
 
-void abort(void)
-	__attribute__((__noreturn__)); // causes an abnormal program termination
-void exit(int status)
-	__attribute__((__noreturn__)); // causes a program to terminate normally
-int system(const char *);          // passes string to command processor
+/* causes an abnormal program termination */
+void abort(void) __attribute__((__noreturn__));
 
-int      abs(int); // returns absolute value
+/* causes a program to terminate normally */
+void exit(int status) __attribute__((__noreturn__));
+
+/* passes string to command processor */
+int system(const char *);
+
+/* returns absolute value */
+int      abs(int);
 long int labs(long int);
 
-int  rand(void); // returns pseudo random number from 0 to RAND_MAX
-void srand(
-	unsigned int seed); // this function seeds the random number generator
-int atoi(const char *str);
+/* returns pseudo random number from 0 to RAND_MAX */
+int rand(void);
+
+/* seeds the random number generator */
+void srand(unsigned int seed);
+
+int   atoi(const char *str);
+char *itoa(int num, char *str, int max);
 
 //@todo: a lot of other functions should be implemented
 
